d4417910f5b1240440fc718556eeea6a8408088a
Adam Naništa <nanista@totalmoney.sk>
2026-04-27 14:51:37 +0200

    add contest_entry cpt

--- wp-content/themes/domaci-milacik/functions.php
+++ wp-content/themes/domaci-milacik/functions.php
@@ -4,8 +4,12 @@
 		exit; // Exit if accessed directly.
 	}
 
+    /**
+     * Save/Load ACF json
+     */
+
     add_filter('acf/settings/save_json', function() {
-        return get_stylesheet_directory() . '/acf-json'; // Save
+        return get_stylesheet_directory() . '/acf-json';
     });
 
     add_filter('acf/settings/load_json', function($paths) {
