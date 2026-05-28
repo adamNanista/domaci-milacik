bc9d7451f05700c5194890ee859b633b506b9132
Adam Naništa <nanista@totalmoney.sk>
2026-05-28 17:30:30 +0200

    slider

--- wp-content/themes/domaci-milacik/blocks/leaderboard/leaderboard.js
+++ wp-content/themes/domaci-milacik/blocks/leaderboard/leaderboard.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	const leaderboardSlider = new Swiper(leaderboardSliderElement, {
 		slidesPerView: "auto",
 		spaceBetween: 12,
+		navigation: {
+			prevEl: "#leaderboard-button-prev",
+			nextEl: "#leaderboard-button-next",
+		},
 		breakpoints: {
 			768: {
 				slidesPerView: "auto",
--- wp-content/themes/domaci-milacik/blocks/leaderboard/render.php
+++ wp-content/themes/domaci-milacik/blocks/leaderboard/render.php
@@ -19,40 +19,50 @@
                     <h2 class="font-heading text-3xl font-bold text-uppercase md:text-4xl">TOP miláčikovia</h2>
                 </div>
                 <div class="container">
-                    <div class="leaderboard-slider swiper" id="leaderboard-slider">
-                        <div class="swiper-wrapper">
-                            <?php 
-                                while ( $query->have_posts() ) {
-                                    $query->the_post();
+                    <div class="leaderboard-slider-wrapper">
+                        <div class="leaderboard-slider swiper" id="leaderboard-slider">
+                            <div class="swiper-wrapper">
+                                <?php 
+                                    while ( $query->have_posts() ) {
+                                        $query->the_post();
 
-                                    $votes = (int) get_field('votes');
+                                        $votes = (int) get_field('votes');
 
-                                    ?>
-                                        <div class="swiper-slide">
-                                            <article class="card">
-                                                <a href="<?php the_permalink(); ?>" class="card-thumbnail">
-                                                    <?php the_post_thumbnail( 'full' ); ?>
-                                                </a>
-                                                <div class="card-content">
-                                                    <div class="space-y-4">
-                                                        <div class="space-y-3">
-                                                            <h3 class="text-2xl font-bold">
-                                                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                                            </h3>
-                                                            <p class="votes votes-lg">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-icon lucide-heart"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg>
-                                                                <?php echo esc_html( $votes ); ?>
-                                                            </p>
+                                        ?>
+                                            <div class="swiper-slide">
+                                                <article class="card">
+                                                    <a href="<?php the_permalink(); ?>" class="card-thumbnail">
+                                                        <?php the_post_thumbnail( 'full' ); ?>
+                                                    </a>
+                                                    <div class="card-content">
+                                                        <div class="space-y-4">
+                                                            <div class="space-y-3">
+                                                                <h3 class="text-2xl font-bold">
+                                                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                                                </h3>
+                                                                <p class="votes votes-lg">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-icon lucide-heart"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg>
+                                                                    <?php echo esc_html( $votes ); ?>
+                                                                </p>
+                                                            </div>
+                                                            <a href="<?php the_permalink(); ?>" class="button button-sm button-wide button-primary">Hlasovať</a>
                                                         </div>
-                                                        <a href="<?php the_permalink(); ?>" class="button button-sm button-wide button-primary">Hlasovať</a>
                                                     </div>
-                                                </div>
-                                            </article>
-                                        </div>
-                                    <?php
-                                }
-                            ?>
+                                                </article>
+                                            </div>
+                                        <?php
+                                    }
+                                ?>
+                            </div>
                         </div>
+                        <button class="swiper-button-prev" id="leaderboard-button-prev" type="button">
+                            <span class="visually-hidden">Predošlý</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
+                        </button>
+                        <button class="swiper-button-next" id="leaderboard-button-next" type="button">
+                            <span class="visually-hidden">Ďalší</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+                        </button>
                     </div>
                 </div>
             </section>
