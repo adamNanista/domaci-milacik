481dcc88b5fb902ff5e89e6b8cfa86c64775f98b
Adam Naništa <nanista@totalmoney.sk>
2026-06-03 16:21:23 +0200

    fix

--- wp-content/themes/domaci-milacik/blocks/steps/render.php
+++ wp-content/themes/domaci-milacik/blocks/steps/render.php
@@ -14,7 +14,7 @@
     <div class="text-center">
         <h2 class="font-heading text-4xl font-bold uppercase tracking-tight"><?php echo $title ? wp_kses_post( $title ) : 'Kroky'; ?></h2>
     </div>
-    <div class="grid gap-6 md:grid-cols-2 md:gap-4 lg:grid-cols-4">
+    <div class="grid gap-8 md:grid-cols-2 md:gap-4 lg:grid-cols-4">
         <?php echo $content; ?>
     </div>
 </section>
